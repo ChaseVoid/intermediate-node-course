e717552219f1550181e502f1f4acd0b5c76f66dc
Ajay Solleti <144334+ChaseVoid@users.noreply.github.com>
2020-03-07 13:45:20 +0530

    connect to mongo with User model

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
+require('dotenv').config()
+
 const express= require('express');
 const mongoose= require('mongoose');
 const bodyParser= require('body-parser');
 const port=8000;
 const app= express();
+
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+mongoose.set('useUnifiedTopology', true);
+
+const User=require('./models/User');
+mongoose.connect(process.env.MONGO_URI_USERDATA)
 
 app.use(bodyParser.json());
 
